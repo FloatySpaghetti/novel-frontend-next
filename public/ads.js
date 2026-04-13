1e3af3a28c8f42795e0cad48dda1a12fa83b1353
FloatySpaghetti <rensiscolim@gmail.com>
2026-04-14 02:04:17 +0700

    Generating static pages before next.js build

--- public/ads.js
+++
@@ -1,2 +0,0 @@
-// public/ads.js
-window.canRunAds = true;
--- public/sw.js
+++
@@ -1,6 +0,0 @@
-self.options = {
-    "domain": "fenoofaussut.net",
-    "zoneId": 9947782
-}
-self.lary = ""
-importScripts('https://fenoofaussut.net/act/files/service-worker.min.js?r=sw')
